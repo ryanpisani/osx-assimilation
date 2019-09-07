0afcdec3adf0ef7e39c4622d92bf85a741d4a025
Ryan P <ryan@MacBook-Pro.local>
2019-09-07 11:30:28 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,14 @@
 #Bootstrap homebrew & xcode
 
 xcode-select --install
+echo -n "Press ENTER to continue once Xcode has been Installed: "
+read ENTER
+echo -e "Installing HomeBrew\n"
+sleep 2
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo -e "Installing Ansible\n"
+sleep 2
 brew install ansible
+if [[ $? -eq 0 ]]; then 
+  echo -e "\nYou may now execute \"ansible-playbook newmac.yaml\"\n"
+f
